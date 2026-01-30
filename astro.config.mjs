1cb884c5fde9c99d0ba629a35c59950f9a560b0e
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-30 20:51:12 -0300

    feat(seo): add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
@@ -37,6 +38,7 @@ export default defineConfig({
 	},
 
 	integrations: [
-		react()
+		react(),
+		sitemap()
 	]
 })
