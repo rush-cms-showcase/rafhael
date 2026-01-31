7c250a6c4a7006d28135505de18902e5515fe06e
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-31 02:36:36 -0300

    chore: ignore .vercel build directory

--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic@_@js.Cd8W1tKP.js';
-export { renderers } from '../renderers.mjs';
--- .vercel/output/static/entry/lazy-search-dialog.Ch1QwkxG.js
+++
@@ -1,1 +0,0 @@
-import{L as i}from"../chunks/lazy-search-dialog.D7UquGDy.js";import"../chunks/jsx-runtime.D_zvdyIk.js";import"../chunks/index.WFquGv8Z.js";import"../chunks/index.ZX95bEFL.js";export{i as default};
